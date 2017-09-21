83537f3254756c7b222d5a25685f54588b4ef4a4
Duncan <duncan@duncans-macbook-pro-2.local>
2017-09-21 00:09:38 -0400

    Initial Commit

---
+++ cpu-render/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpu-render
+//
+//  Created by Duncan on 9/21/17.
+//  Copyright © 2017 Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
